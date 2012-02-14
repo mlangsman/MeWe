4671a401aaa0c88c175a71cb82ec8b1868784c60
Matt <matt@zurb.com>
2012-02-13 16:40:16 -0800

    More fixes. Resize menu list to be the height of the tallest sub menu

--- javascripts/foundation.top-bar.js
+++ javascripts/foundation.top-bar.js
@@ -10,7 +10,7 @@
     if (onMobile()) {
       event.preventDefault();
 
-      $(this).closest('.attached').toggleClass('expanded');
+      $(this).closest('.top-bar').toggleClass('expanded');
     }
   });
   
@@ -20,7 +20,9 @@
           $selectedLi = $this.closest('li'),
           $nextLevelUl = $selectedLi.find('>ul'),
           $titleLi = $('<li class="title show-on-phones js-generated"><h5></h5></li>'),
-          $attached = $this.closest('.attached');
+          $attached = $this.closest('.attached'),
+          $topbar = $this.closest('.top-bar'),
+          $largestUl;
 
       event.preventDefault();
       
@@ -34,13 +36,25 @@
       $titleLi.find('h5').html($selectedLi.children('a').first().html());
       $nextLevelUl.prepend($titleLi);
       $nextLevelUl.prepend('<li class="back show-on-phones js-generated"><a href="">&larr; Back</a></li>');
+      
+      if (currentIndex === 1) {
+        $largestUl = $nextLevelUl;
+        $nextLevelUl.find('ul.dropdown').each(function () {
+          if ($(this).height() > $largestUl.height()) {
+            $largestUl = $(this);
+          }
+        });
+        $attached.css({'height': $largestUl.height() + 45 + 'px'});
+      }
     }
   });
   
   $('.top-bar .has-dropdown .back').live('click', function (event) {
     var $this = $(this),
         $activeLi = $this.closest('li.active'),
-        $attached = $this.closest('.attached');
+        $attached = $this.closest('.attached'),
+        $topbar = $this.closest('.top-bar'),
+        $previousLevelUl = $activeLi.closest('ul');
     
     event.preventDefault();
     
@@ -48,6 +62,10 @@
     
     $attached.css({'left': '-' + 100 * currentIndex + '%'});
     $attached.find('>.name').css({'left': 100 * currentIndex + '%'});
+    
+    if (currentIndex === 0) {
+      $attached.css({'height': ''});
+    }
     
     setTimeout(function () {
       $activeLi.removeClass('active');
