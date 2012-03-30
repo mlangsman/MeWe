deb737f10702e652a076e809774dc50446c1ea95
Marc Langsman <marc@langsman.com>
2012-03-30 13:53:45 +0100

    Implemented jquery accordion on events page

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,37 @@ jQuery(document).ready(function ($) {
 
 	/* Use this js doc for all application specific JS */
 
+
+	//ACCORDION BUTTON ACTION (ON CLICK DO THE FOLLOWING)
+	$('.accordionButton').click(function() {
+		//REMOVE THE ON CLASS FROM ALL BUTTONS
+		$('.accordionButton').removeClass('open');
+		//NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
+		$('.accordionContent').slideUp('normal');
+		//IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
+			if($(this).next().is(':hidden') == true) 
+			{
+					//ADD THE ON CLASS TO THE BUTTON
+					$(this).addClass('open');
+					//OPEN THE SLIDE
+					$(this).next().slideDown('normal');
+			}
+		});
+	/*** REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+	//ADDS THE .OVER CLASS FROM THE STYLESHEET ON MOUSEOVER
+	$('.accordionButton').mouseover(function() {
+	$(this).addClass('over');
+	//ON MOUSEOUT REMOVE THE OVER CLASS
+	}).mouseout(function() {
+	$(this).removeClass('over');
+	});
+	/*** END REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+	/********************************************************************************************************************
+	CLOSES ALL S ON PAGE LOAD
+	********************************************************************************************************************/
+	$('.accordionContent').hide();
+
+
 	/* TABS --------------------------------- */
 	/* Remove if you don't need :) */
 
